8333c220cbec4093495b60f27c3b9a494178a194
nayeemmehedi <boyneel79@gmail.com>
2024-10-21 08:37:50 +0600

    error

--- app/error.tsx
+++ app/error.tsx
@@ -1,24 +1,25 @@
-"use client"
+// src/app/error.js
+"use client";
+import { useEffect } from "react";
 
-import React from "react";
-import error from "/public/illustration/error.jpg";
-import Image from "next/image";
-import Link from "next/link";
+export default function ErrorPage({ error, reset}:any) {
+  useEffect(() => {
+    // Log the error to an error reporting service, if needed
+    console.error(error);
+  }, [error]);
 
-function PaymentSuccess() {
   return (
-    <div>
-      <Link href="/">
-        <button className="font-extralight text-xs p-3 m-4 border border-green-200 ">
-          Back To Home
-        </button>
-      </Link>
-
-      <div className="w-[50%] ml-auto mr-auto">
-        <Image src={error} alt="error"></Image>
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold text-red-500">Something went wrong!</h1>
+      <p className="mt-2 text-gray-600">
+        {error?.message || "An unexpected error occurred."}
+      </p>
+      <button
+        onClick={() => reset()} // Attempt to recover by reloading the current route
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+      >
+        Try Again
+      </button>
     </div>
   );
 }
-
-export default PaymentSuccess;
