7a299708d6016b8abc1712863f96d52f658163e3
nayeemmehedi <boyneel79@gmail.com>
2024-10-23 11:59:25 +0600

    error issue

--- app/error.tsx
+++ app/error.tsx
@@ -15,8 +15,8 @@ export default function ErrorPage({ error, reset}:any) {
         {error?.message || "An unexpected error occurred."}
       </p>
       <button
-        onClick={() => reset()} // Attempt to recover by reloading the current route
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+        onClick={() => window.location.reload()} // Attempt to recover by reloading the current route
+        className="mt-4 px-4 py-2 bg-green-900 text-white rounded"
       >
         Try Again
       </button>
